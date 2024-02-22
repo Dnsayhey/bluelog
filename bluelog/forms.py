b4c62625e05419f10a4ce4cf2cb753204548c88c
dnsayhey <yanlei921@live.com>
2024-02-23 00:05:02 +0800

    Second commit

---
+++ bluelog/forms.py
@@ -0,0 +1,65 @@
+from flask_wtf import FlaskForm
+from flask_ckeditor import CKEditorField
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField,
+    SelectField,
+    TextAreaField,
+    HiddenField,
+)
+from wtforms.validators import (
+    DataRequired,
+    Length,
+    ValidationError,
+    Email,
+    URL,
+    Optional,
+)
+
+from bluelog.models import Category
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(6, 32)])
+    remember = BooleanField("Remember me", default=False)
+    submit = SubmitField("Log in")
+
+
+class CategoryForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(), Length(1, 30)])
+    submit = SubmitField("Create")
+
+    def validate_name(self, field):
+        if Category.query.filter_by(name=field.data).first():
+            raise ValidationError("Name already in use.")
+
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(1, 60)])
+    body = CKEditorField("Body", validators=[DataRequired()])
+    category = SelectField("Category", coerce=int, default=1)
+    submit = SubmitField("Post")
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.category.choices = [
+            (category.id, category.name)
+            for category in Category.query.order_by(Category.id).all()
+        ]
+
+
+class CommentForm(FlaskForm):
+    author = StringField("Author", validators=[DataRequired(), Length(1, 30)])
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(1, 254)])
+    site = StringField("Site", validators=[Optional(), URL(), Length(0, 255)])
+    body = TextAreaField("Comment", validators=[DataRequired()])
+    submit = SubmitField()
+
+
+class AdminCommentForm(CommentForm):
+    author = HiddenField()
+    email = HiddenField()
+    site = HiddenField()
--- bluelog/views/blog.py
+++ bluelog/views/blog.py
@@ -1,8 +1,49 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, current_app
+
+from bluelog.models import Admin, Category, Post
 
 blog_bp = Blueprint("blog", __name__)
 
 
 @blog_bp.route("/")
 def index():
-    return render_template("blog/index.html")
+    page = request.args.get("page", 1, type=int)
+    per_page = current_app.config.get("BLUELOG_POST_PER_PAGE", 10)
+
+    admin = Admin.query.first()
+    categories = Category.query.all()
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=per_page
+    )
+    posts = pagination.items
+
+    return render_template(
+        "blog/index.html",
+        admin=admin,
+        categories=categories,
+        pagination=pagination,
+        posts=posts,
+    )
+
+
+@blog_bp.route("/category/<int:category_id>")
+def show_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    categories = Category.query.all()
+    page = request.args.get("page", 1, type=int)
+    per_page = current_app.config.get("BLUELOG_POST_PER_PAGE", 10)
+    pagination = (
+        Post.query.filter_by(category_id=category_id)
+        .order_by(Post.timestamp.desc())
+        .paginate(page=page, per_page=per_page)
+    )
+    posts = pagination.items
+    return render_template(
+        "blog/category.html", category=category, pagination=pagination, posts=posts, categories=categories
+    )
+
+
+@blog_bp.route("/post/<int:post_id>")
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("blog/post.html", post=post)
